7abdaf4f11da1c7c337ed0db2772ad8b9bf3563b
Maanav Garg <maanavgarg@icloud.com>
2019-02-19 23:23:45 -0500

    database print

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,8 +1,12 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds']
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('database_key.json', scope)
 client = gspread.authorize(creds)
 
 sheet = client.open('Database').sheet1
+
+database = sheet.get_all_records()
+print(database)
