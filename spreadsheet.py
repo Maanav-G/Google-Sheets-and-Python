1663bd8b2ca43b4ed63180fd006b4957e7c2528c
Maanav Garg <maanavgarg@icloud.com>
2019-02-19 23:37:38 -0500

    added specific functions

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,5 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import pprint 
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
@@ -8,5 +9,30 @@ client = gspread.authorize(creds)
 
 sheet = client.open('Database').sheet1
 
+pp = pprint.PrettyPrinter()
+
+# Use for all records
 database = sheet.get_all_records()
-print(database)
+
+# Use to get a row, col or cell
+# database = sheet.row_values(6)
+# database = sheet.col_values(6)
+# database = sheet.cell(6,2).value
+
+# pp.pprint(database)
+
+# Use to update a specific cell
+# sheet.update_cell(6,2, 'Maanav')
+# database = sheet.cell(6,2).value
+
+# pp.pprint(database)
+
+# Use to add row
+# row = ["I'm", "updating", "this", "spreadsheet", "from", "python"]
+# index = 2
+# sheet.insert_row(row,index)
+
+# Used to delete specfic row
+# sheet.delete_row(2)
+
+print(sheet.row_count)
